fb812a0921cd281fb2c0b29c021cfa0774b20d4c
Crashber <95576064+Crashber@users.noreply.github.com>
2021-12-12 14:39:48 +0100

    vue material test6

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <md-toolbar class="md-primary">
+        <md-title> Martins Webseite </md-title>
+        
+    </md-toolbar>
+
+    <div id="app">
+      <HelloWorld></HelloWorld>
+      <Table></Table>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Table from './components/Table.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Table
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
